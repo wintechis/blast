da70ff16b3ecd0cd626d8f5a96c1206be4df30fb
derwehr <derwehr@gmail.com>
2021-08-19 17:12:00 +0200

    add service worker, closes #76

---
+++ workbox-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  globDirectory: '.',
+  globPatterns: [
+    '.',
+    'index.html',
+    'style.css',
+    'js/**/*.js',
+    'media/**',
+  ],
+  swSrc: 'sw-blank.js',
+  swDest: 'sw.js',
+};
