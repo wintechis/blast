3d02347894eec31708dbaf0b2af85a83d01d9557
Thomas Wehr <derwehr@gmail.com>
2021-09-30 12:47:12 +0200

    finish joycon implementation, closes #41

--- src/things/joycon/blocks.js
+++ src/things/joycon/blocks.js
@@ -16,14 +16,125 @@ Blockly.Blocks['joycon_read_property'] = {
         .setCheck('Thing')
         .appendField('read')
         .appendField(new Blockly.FieldDropdown([
-          ['gyroscope', 'Gyroscope'],
-        ]), 'property')
+          ['accelerometers', 'accelerometers'],
+          ['actual accelerometer', 'actualAccelerometer'],
+          ['actual gyroscope', 'actualGyroscope'],
+          ['actual orientation', 'actualOrientation'],
+          ['actual orientation quaternion', 'actualOrientationQuaternion'],
+          ['gyroscopes', 'gyroscopes'],
+          ['quaternion', 'quaternion'],
+        ], this.propertyValidator), 'property')
         .appendField('property of Nintendo JoyCon');
+    this.setInputsInline(false);
     this.setOutput(true, ['String', 'Number']);
     this.setColour(255);
     this.setTooltip('Reads a property of a Nintendo JoyCon controller.');
     this.setHelpUrl('');
     this.firstTime = true;
+  },
+  propertyValidator: function(option) {
+    this.getSourceBlock().updateInputs(option);
+    return option;
+  },
+  /**
+   * Adds dropdowns for property sub values based on the selected property.
+   * @param {String} property The property to add dropdowns for.
+   */
+  updateInputs: function(property) {
+    this.removeInput('propertySubValue', true);
+    this.removeInput('propertySubValue2', true);
+    this.removeInput('propertySubValue3', true);
+
+    if (property === 'accelerometers') {
+      this.appendDummyInput('propertySubValue')
+          .appendField('accelerometer')
+          .appendField(new Blockly.FieldDropdown([
+            ['0', '0'],
+            ['1', '1'],
+            ['2', '2'],
+          ]), 'propertySubValue');
+      this.appendDummyInput('propertySubValue2')
+          .appendField('axis')
+          .appendField(new Blockly.FieldDropdown([
+            ['x', 'x'],
+            ['y', 'y'],
+            ['z', 'z'],
+          ]), 'propertySubValue2');
+    }
+    if (property === 'actualAccelerometer') {
+      this.appendDummyInput('propertySubValue')
+          .appendField('axis')
+          .appendField(new Blockly.FieldDropdown([
+            ['x', 'x'],
+            ['y', 'y'],
+            ['z', 'z'],
+          ]), 'propertySubValue');
+    }
+    if (property === 'actualGyroscope') {
+      this.appendDummyInput('propertySubValue')
+          .appendField('unit')
+          .appendField(new Blockly.FieldDropdown([
+            ['rps', 'rps'],
+            ['dps', 'dps'],
+          ]), 'propertySubValue');
+      this.appendDummyInput('propertySubValue2')
+          .appendField('axis')
+          .appendField(new Blockly.FieldDropdown([
+            ['x', 'x'],
+            ['y', 'y'],
+            ['z', 'z'],
+          ]), 'propertySubValue2');
+    }
+    if (property === 'actualOrientation') {
+      this.appendDummyInput('propertySubValue')
+          .appendField('degrees')
+          .appendField(new Blockly.FieldDropdown([
+            ['alpa', 'alpha'],
+            ['beta', 'beta'],
+            ['gamma', 'gamma'],
+          ]), 'propertySubValue');
+    }
+    if (property === 'actualOrientationQuaternion') {
+      this.appendDummyInput('propertySubValue')
+          .appendField('degrees')
+          .appendField(new Blockly.FieldDropdown([
+            ['alpa', 'alpha'],
+            ['beta', 'beta'],
+            ['gamma', 'gamma'],
+          ]), 'propertySubValue');
+    }
+    if (property === 'gyroscopes') {
+      this.appendDummyInput('propertySubValue')
+          .appendField('gyroscope')
+          .appendField(new Blockly.FieldDropdown([
+            ['0', '0'],
+            ['1', '1'],
+            ['2', '2'],
+          ]), 'propertySubValue');
+      this.appendDummyInput('propertySubValue2')
+          .appendField('axis')
+          .appendField(new Blockly.FieldDropdown([
+            ['x', '0'],
+            ['y', '1'],
+            ['z', '2'],
+          ]), 'propertySubValue2');
+      this.appendDummyInput('propertySubValue3')
+          .appendField('unit')
+          .appendField(new Blockly.FieldDropdown([
+            ['dps', 'dps'],
+            ['rps', 'rps'],
+          ]), 'propertySubValue3');
+    }
+    if (property === 'quaternion') {
+      this.appendDummyInput('propertySubValue')
+          .appendField('axis')
+          .appendField(new Blockly.FieldDropdown([
+            ['w', 'w'],
+            ['x', 'x'],
+            ['y', 'y'],
+            ['z', 'z'],
+          ]), 'propertySubValue');
+    }
   },
 };
 
--- src/things/joycon/generators.js
+++ src/things/joycon/generators.js
@@ -13,13 +13,16 @@
   */
 Blockly.JavaScript['joycon_read_property'] = function(block) {
   const property = block.getFieldValue('property');
+  const sub = block.getFieldValue('propertySubValue') || '';
+  const sub2 = block.getFieldValue('propertySubValue2') || '';
+  const sub3 = block.getFieldValue('propertySubValue3') || '';
   const id = Blockly.JavaScript.valueToCode(
       block,
       'Thing',
       Blockly.JavaScript.ORDER_NONE,
   );
 
-  const code = `readJoyConProperty(${id}, '${property}');\n`;
+  const code = `readJoyConProperty(${id}, '${property}', '${sub}', '${sub2}', '${sub3}')`;
   return [code, Blockly.JavaScript.ORDER_NONE];
 };
 
@@ -27,10 +30,14 @@ Blockly.JavaScript['joycon_read_property'] = function(block) {
  * Reads a property of a Joy-Con.
  * @param {string} id the id of the Joy-Con.
  * @param {string} property the property to read.
+ * @param {string} subValue first sub level of the property.
+ * @param {string} subValue2 second sub level of the property.
+ * @param {string} subValue3 third sub level of the property.
+ * @param {JSInterpreter.AsyncCallback} callback JS Interpreter callback.
  * @returns {string} the value of the property.
  * @private
  */
-const readJoyConProperty = async function(id, property) {
+const readJoyConProperty = async function(id, property, subValue, subValue2, subValue3, callback) {
   const device = Blast.Things.webHidDevices.get(id);
   if (!device.opened) {
     try {
@@ -40,17 +47,44 @@ const readJoyConProperty = async function(id, property) {
     }
   }
 
-  const joyCon = connectedJoyCons.get(device.productId);
+  let joyCon;
+  if (device.productId === 0x2006) {
+    joyCon = new JoyConLeft(device);
+  } else if (device.productId === 0x2007) {
+    joyCon = new JoyConRight(device);
+  }
+  await joyCon.open();
+  await joyCon.enableStandardFullMode();
+  await joyCon.enableIMUMode();
+
+  const hidInputHandler = async function(event) {
+    const packet = event.detail;
+    if (!packet || !packet.actualOrientation) {
+      return;
+    }
+    
+    console.log(packet);
+    joyCon.removeEventListener('hidinput', hidInputHandler);
+    if (subValue2 !== '') {
+      if (property === 'accelerometers') {
+        callback(packet[property][subValue][subValue2].acc);
+      } else if (property === 'gyroscopes') {
+        callback(packet[property][subValue][subValue2][subValue3]);
+      } else {
+        callback(packet[property][subValue][subValue2]);
+      }
+    } else {
+      callback(packet[property][subValue]);
+    }
+  };
 
   if (!joyCon.eventListenerAttached) {
     await joyCon.open();
     await joyCon.enableStandardFullMode();
     await joyCon.enableIMUMode();
     await joyCon.enableVibration();
-    joyCon.addEventListener('hidinput', (event) => {
-      console.log(joyCon);
-      console.log(event.detail);
-    });
+    joyCon.addEventListener('hidinput', hidInputHandler);
+
     joyCon.eventListenerAttached = true;
   }
 };
