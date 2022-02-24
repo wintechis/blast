430db141a704c68816d7fd4b381ca4c0a53b7c94
derwehr <derwehr@gmail.com>
2022-02-24 14:10:18 +0100

    prettify

--- src/things/index.ts
+++ src/things/index.ts
@@ -1,28 +1,31 @@
-import * as WoT from "wot-typescript-definitions";
-import { Servient } from "@node-wot/core";
-
+// eslint-disable-next-line node/no-unpublished-import
+import * as WoT from 'wot-typescript-definitions';
+// eslint-disable-next-line node/no-unpublished-import
+import {Servient} from '@node-wot/core';
 
 let servient: Servient;
 let wot: typeof WoT;
-let things: { [key: string]: WoT.ExposedThing } = {};
+const things: {[key: string]: WoT.ExposedThing} = {};
 
-export const getServient = function(): Servient {
-    if (!servient) {
-        servient = new Servient();
-    }
-    return servient;
+export const getServient = function (): Servient {
+  if (!servient) {
+    servient = new Servient();
+  }
+  return servient;
 };
 
-export const getWoT = async function(): Promise<typeof WoT> {
-    if (!wot) {
-        wot = await getServient().start();
-    }
-    return wot;
+export const getWoT = async function (): Promise<typeof WoT> {
+  if (!wot) {
+    wot = await getServient().start();
+  }
+  return wot;
 };
 
-export const getThing = async function(td: WoT.ThingDescription): Promise<WoT.ExposedThing> {
-    if (!things[td.id]) {
-        things[td.id] = await (await getWoT()).produce(td);
-    }
-    return things[td.id];
-}
+export const getThing = async function (
+  td: WoT.ThingDescription
+): Promise<WoT.ExposedThing> {
+  if (!things[td.id]) {
+    things[td.id] = await (await getWoT()).produce(td);
+  }
+  return things[td.id];
+};
