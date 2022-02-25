db233b105992a5a2cf949e7c393795efadf5fbda
derwehr <derwehr@gmail.com>
2022-02-25 15:10:27 +0100

    statisfy linter

--- src/thingBlocks/streamdeck/generators.js
+++ src/thingBlocks/streamdeck/generators.js
@@ -14,7 +14,6 @@ import {getThingsLog} from './../../blast_things.js';
 import {apiFunctions} from './../../blast_interpreter.js';
 import {asyncApiFunctions} from './../../blast_interpreter.js';
 import {getInterpreter} from './../../blast_interpreter.js';
-import {getWebHidDevice} from './../../blast_things.js';
 import {setInterrupted} from './../../blast_interpreter.js';
 // eslint-disable-next-line node/no-missing-import
 import {Streamdeck} from './../../things/streamdeck/Streamdeck.js';
