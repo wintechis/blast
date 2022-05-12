cbb1e5702d5e314ebab0e720aaef95584a878a26
Freund, Michael <michael.freund@iis.fraunhofer.de>
2022-05-12 07:53:30 +0200

    update imports in cli from src to dist

--- examples/cli/index.js
+++ examples/cli/index.js
@@ -7,24 +7,24 @@
 'use strict';
 
 import Blockly from 'blockly';
-import {initInterpreter} from '../../src/blast_interpreter.js';
-import {initStatesInterpreter} from '../../src/blast_states_interpreter.js';
-import {getStdInfo} from '../../src/blast_interpreter.js';
- 
+import {initInterpreter} from '../../dist/blast_interpreter.js';
+import {initStatesInterpreter} from '../../dist/blast_states_interpreter.js';
+import {getStdInfo} from '../../dist/blast_interpreter.js';
+
 // import block blast files to include them in bundled code.
-import '../../src/blocks/all.js';
-import '../../src/generators/all.js';
-import '../../src/things/all.js';
+import '../../dist/blocks/all.js';
+import '../../dist/generators/all.js';
+import '../../dist/thingBlocks/all.js';
 
 /**
  * Initialize Blast. Called on page load.
  * @public
  */
-const init = function() {
+const init = function () {
   const workspace = new Blockly.Workspace();
   initInterpreter(workspace);
   initStatesInterpreter(workspace);
-  
+
   // Display output hint
   const stdInfo = getStdInfo();
   stdInfo('Actionblock output will be displayed here');
