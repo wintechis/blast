2cc3093d31ee6109fc8b5a02f0bb0d2bd038db5d
derwehr <derwehr@gmail.com>
2022-06-19 21:31:43 +0200

    clean-up and convert to interpreter-less blast

--- src/thingBlocks/text2code/blocks.js
+++ src/thingBlocks/text2code/blocks.js
@@ -1,23 +1,22 @@
 'use strict';
 
-import {Blocks, FieldDropdown, FieldMultilineInput} from 'blockly';
+import {Blocks} from 'blockly';
 import {addBlock} from './../../blast_toolbox.js';
 
-
 Blocks['text_to_code'] = {
-    /**
-     * Block for convertubf a text input into js code
-     */
-  init: function() {
-    this.appendValueInput("text")
-        .setCheck('String')
-        .appendField("text_to_code");
+  /**
+   * Block for converting a text input into js code.
+   */
+  init: function () {
+    this.appendValueInput('text')
+      .setCheck('String')
+      .appendField('generate code from text');
     this.setOutput(true, null);
-    this.setColour(230);
- this.setTooltip("");
- this.setHelpUrl("");
-  }
+    this.setColour(0);
+    this.setTooltip('');
+    this.setHelpUrl('');
+  },
 };
 
 // Add the block to the toolbox.
-addBlock("text_to_code", "actions");
+addBlock('text_to_code', 'actions');
--- src/thingBlocks/text2code/generators.js
+++ src/thingBlocks/text2code/generators.js
@@ -7,128 +7,128 @@ import targetWord2idx from './mappings/target-word2idx.js';
 import targetIdx2word from './mappings/target-idx2word.js';
 
 import Blockly from 'blockly';
-import {asyncApiFunctions, getInterpreter,throwError} from './../../blast_interpreter.js';
 
-  /**
+/**
  * Generates JavaScript code for the text_to_code block.
  * @param {Blockly.Block} block the text_to_code block.
  * @returns {String} the generated code.
  */
-Blockly.JavaScript['text_to_code'] = function(block) {
-    // inputText
-    let text = Blockly.JavaScript.valueToCode(block, 'text', Blockly.JavaScript.ORDER_ATOMIC); // return string
-   // parsed code outpt
-    //let text = block.getFieldValue('text');
-    //const textNew = Blockly.JavaScript.valueToCode(block, 'text', Blockly.JavaScript.ORDER_NONE);
-    
-    console.log(typeof text);
-    console.log(text);
-    //console.log(typeof textNew);
-    //console.log(textNew);
+Blockly.JavaScript['text_to_code'] = function (block) {
+  const text = Blockly.JavaScript.valueToCode(
+    block,
+    'text',
+    Blockly.JavaScript.ORDER_ATOMIC
+  );
 
-    const code = `textToCode(${text})`;
-    return [code, Blockly.JavaScript.ORDER_NONE];
-  };
+  const code = `await textToCode(${text})`;
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
 
+/**
+ * Generates JavaScript code from text input.
+ * @param {String} text the text input
+ * @returns {String} the generated code.
+ */
+globalThis['textToCode'] = async function (text) {
+  const encoder = await tf.loadLayersModel(
+    '../../dist/thingBlocks/text2code/tfjs_model/encoder/model.json'
+  );
+  const decoder = await tf.loadLayersModel(
+    '../../dist/thingBlocks/text2code/tfjs_model/decoder/model.json'
+  );
 
-// main funtion takes user input text and return parsed code
-const textToCode = async function (text, callback) {
-    console.log(typeof text);
-    console.log(text);
-    const encoder = await tf.loadLayersModel('/src/thingBlocks/text2code/tfjs_model/encoder/model.json')
-    const decoder = await tf.loadLayersModel('/src/thingBlocks/text2code/tfjs_model/decoder/model.json')
+  const inputTensor = convertSentenceToTensor(text);
+  const states = encoder.predict(inputTensor);
+  decoder.layers[1].resetStates(states);
 
-    let inputText = text;
-    //console.log(inputText);
-    //console.log(typeof text);
-    const inputTensor = convertSentenceToTensor(text);
-    const states = encoder.predict(inputTensor);
-    decoder.layers[1].resetStates(states);
+  const responseTokens = [];
+  let terminate = false;
+  let nextTokenID = targetWord2idx['<SOS>'];
+  let numPredicted = 0;
 
-    let responseTokens = [];
-    let terminate = false;
-    let nextTokenID = targetWord2idx['<SOS>'];
-    let numPredicted = 0;
-
-    while (!terminate) {
-        const outputTokenTensor = tf.tidy(() => {
-            const input = generateDecoderInputFromTokenID(nextTokenID);
-                const prediction = decoder.predict(input);
-                return prediction.squeeze().argMax();
-        });
-
-        const outputToken = await outputTokenTensor.data();
-        outputTokenTensor.dispose();
-        nextTokenID = Math.round(outputToken[0]);
-        const word = targetIdx2word[nextTokenID];
-        numPredicted++;
-        console.log(outputToken, nextTokenID, word);
-
-        if (word !== '<EOS>' && word !== '<SOS>') {
-            responseTokens.push(word);
-        }
-
-        if (word === '<EOS>'
-            || numPredicted >= wordContext.decoder_max_seq_length) {
-            terminate = true;
-        }
-        // tf.nextFrame() returns a promise which determines at which a requestAnimationFrame has been terminated.
-        await tf.nextFrame();
-    }
-
-
-    states[0].dispose();
-    states[1].dispose();
-
-    callback (convertTokensToSentence(responseTokens))
-  }
-
-
-// convert input text into tensors
-function convertSentenceToTensor(text) {
-    let inputWordIds = [];
-    let textArray = text.toString().split(' ');
-    console.log(text);
-    console.log(typeof text);
-    console.log(typeof textArray);
-
-    textArray.map((x) => {
-        x = x.toLowerCase();
-        let idx = '1'; // '1' index for UNK
-        if (x in inputWord2idx) {
-            idx = inputWord2idx[x];
-        }
-        inputWordIds.push(Number(idx));
+  while (!terminate) {
+    const outputTokenTensor = tf.tidy(() => {
+      const input = generateDecoderInputFromTokenID(nextTokenID);
+      const prediction = decoder.predict(input);
+      return prediction.squeeze().argMax();
     });
 
-    if (inputWordIds.length < wordContext.encoder_max_seq_length) {
-        inputWordIds =
-            Array.concat(
-                new Array(
-                    wordContext.encoder_max_seq_length - inputWordIds.length + 1)
-                    .join('0').split('').map(Number),
-                inputWordIds
-            ); // '0' index for PAD
-    } else {
-        inputWordIds = inputWordIds.slice(0, wordContext.encoder_max_seq_length);
+    const outputToken = await outputTokenTensor.data();
+    outputTokenTensor.dispose();
+    nextTokenID = Math.round(outputToken[0]);
+    const word = targetIdx2word[nextTokenID];
+    numPredicted++;
+
+    if (word !== '<EOS>' && word !== '<SOS>') {
+      responseTokens.push(word);
     }
-    console.log(inputWordIds);
-    return tf.tensor2d(inputWordIds, [1, wordContext.encoder_max_seq_length]);
+
+    if (
+      word === '<EOS>' ||
+      numPredicted >= wordContext.decoder_max_seq_length
+    ) {
+      terminate = true;
+    }
+
+    await tf.nextFrame();
   }
 
-// concantate each predicted tokens into string
+  states[0].dispose();
+  states[1].dispose();
+
+  const sentence = convertTokensToSentence(responseTokens);
+  return sentence;
+};
+
+/**
+ * Converts a sentence to a tensor.
+ * @param {String} sentence the sentence.
+ * @returns {tf.Tensor} the tensor.
+ */
+function convertSentenceToTensor(sentence) {
+  let inputWordIds = [];
+  const textArray = sentence.toString().split(' ');
+
+  textArray.map(x => {
+    x = x.toLowerCase();
+    let idx = '1'; // '1' index for UNK
+    if (x in inputWord2idx) {
+      idx = inputWord2idx[x];
+    }
+    inputWordIds.push(Number(idx));
+  });
+
+  if (inputWordIds.length < wordContext.encoder_max_seq_length) {
+    inputWordIds = Array.concat(
+      new Array(wordContext.encoder_max_seq_length - inputWordIds.length + 1)
+        .join('0')
+        .split('')
+        .map(Number),
+      inputWordIds
+    ); // '0' index for PAD
+  } else {
+    inputWordIds = inputWordIds.slice(0, wordContext.encoder_max_seq_length);
+  }
+
+  return tf.tensor2d(inputWordIds, [1, wordContext.encoder_max_seq_length]);
+}
+
+/**
+ * Converts tokens to a sentence, by concatenating them.
+ * @param {String[]} tokens the tokens.
+ * @returns {String} the sentence.
+ */
 function convertTokensToSentence(tokens) {
-      return tokens.join(' ');
-  }
- 
-// decoder current token output will be input for next time stamp
+  return tokens.join(' ');
+}
+
+/**
+ * Generates decoder input from a token ID.
+ * @param {Number} tokenID the token ID.
+ * @returns {tf.Tensor} the tensor.
+ */
 function generateDecoderInputFromTokenID(tokenID) {
-      const buffer = tf.buffer([1, 1, wordContext.num_decoder_tokens]);
-      buffer.set(1, 0, 0, tokenID);
-      return buffer.toTensor();
-  }
- 
-  
-asyncApiFunctions.push(['textToCode', textToCode]);
-
-
+  const buffer = tf.buffer([1, 1, wordContext.num_decoder_tokens]);
+  buffer.set(1, 0, 0, tokenID);
+  return buffer.toTensor();
+}
