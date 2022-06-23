b00a72690001ec3a15fa320fdd69a77c47000d0e
Thomas Wehr <derwehr@gmail.com>
2022-06-23 16:01:44 +0200

    remove mobile version

--- examples/web/gulpfile.js
+++ examples/web/gulpfile.js
@@ -1,16 +1,6 @@
 import gulp from 'gulp';
-import workboxBuild from 'workbox-build';
 import git from 'git-rev-sync';
 import replace from 'gulp-replace';
-
-gulp.task('workbox', () => {
-  return workboxBuild.injectManifest({
-    globDirectory: './',
-    globPatterns: ['src/**'],
-    swSrc: 'sw-pre-workbox.js',
-    swDest: 'sw.js',
-  });
-});
 
 gulp.task('update-version', () => {
   const version = git.short('../../');
