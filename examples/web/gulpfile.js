89c5059e51a6cda1e0d88c40599bc65e99707474
derwehr <derwehr@gmail.com>
2022-06-25 00:05:26 +0200

    fix not signaling async completion

--- examples/web/gulpfile.js
+++ examples/web/gulpfile.js
@@ -2,15 +2,14 @@ import gulp from 'gulp';
 import git from 'git-rev-sync';
 import replace from 'gulp-replace';
 
-gulp.task('update-version', () => {
+gulp.task('update-version', async () => {
   const version = git.short('../../');
-  gulp
+  await gulp
     .src('./dist/app.js')
     .pipe(replace(/\{\{commit_hash\}\}/g, version))
     .pipe(gulp.dest('./dist/'));
-  gulp
+  await gulp
     .src('index.html')
-    // regular expression to match the commit hash in dist/app.js?v=#b096213
     .pipe(replace(/dist\/app\.js\?v=#\w+/g, `dist/app.js?v=${version}`))
     .pipe(gulp.dest('.'));
   return;
