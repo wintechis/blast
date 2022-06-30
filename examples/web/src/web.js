b77c68dfe5a9962a60fb2f85cc36a8fa0e397024
derwehr <derwehr@gmail.com>
2022-06-30 17:01:09 +0200

    add pseudo code tab and `toPseudoCode` extension

--- examples/web/src/web.js
+++ examples/web/src/web.js
@@ -9,6 +9,7 @@
 import Blockly from 'blockly';
 import {
   getLatestCode,
+  getWorkspace,
   onStatusChange,
   runJS,
   setStdIn,
@@ -28,13 +29,14 @@ import {
   setWebHidButtonHandler,
 } from '../../../dist/blast_things.js';
 import {requestDevice} from '../../../dist/blast_webBluetooth.js';
+const {ASTNode, BlockSvg, constants, WorkspaceSvg} = Blockly;
 
 /**
  * List of tab names.
  * @type {Array.<string>}
  * @private
  */
-export const TABS_ = ['workspace', 'javascript', 'xml', 'deviceLogs'];
+export const TABS_ = ['workspace', 'javascript', 'pseudo', 'xml', 'deviceLogs'];
 
 /**
  * Name of currently selected tab.
@@ -335,6 +337,11 @@ const renderContent_ = function () {
   if (typeof PR === 'object') {
     PR.prettyPrint();
   }
+
+  // render the pseudo code content.
+  const contentPseudo = document.getElementById('content_pseudo');
+  const ws = getWorkspace();
+  contentPseudo.textContent = ws.toPseudoCode();
 };
 
 /**
@@ -607,4 +614,138 @@ export const initUi = function (ws) {
   window.setTimeout(importPrettify, 1);
 
   setStatus(statusValues.READY);
+};
+
+/**
+ * Converts the workspace to a pseudo code string.
+ * @param {string=} opt_emptyToken The placeholder string used to denote an
+ *     empty field. If not specified, '?' is used.
+ * @return {string} The pseudo code string.
+ */
+BlockSvg.prototype.toPseudoCode = function (opt_emptyToken) {
+  let pseudo = [];
+  const emptyFieldPlaceholder = opt_emptyToken || '?';
+
+  // Temporarily set flag to navigate to all fields.
+  const prevNavigateFields = ASTNode.NAVIGATE_ALL_FIELDS;
+  ASTNode.NAVIGATE_ALL_FIELDS = true;
+
+  let node = ASTNode.createBlockNode(this);
+  const rootNode = node;
+
+  /**
+   * Whether or not to add parentheses around an input.
+   * @param {!Connection} connection The connection.
+   * @return {boolean} True if we should add parentheses around the input.
+   */
+  function shouldAddParentheses(connection) {
+    let checks = connection.getCheck();
+    if (!checks && connection.targetConnection) {
+      checks = connection.targetConnection.getCheck();
+    }
+    return (
+      !!checks &&
+      (checks.indexOf('Boolean') !== -1 || checks.indexOf('Number') !== -1)
+    );
+  }
+
+  /**
+   * Check that we haven't circled back to the original root node.
+   */
+  function checkRoot() {
+    if (
+      node &&
+      node.getType() === rootNode.getType() &&
+      node.getLocation() === rootNode.getLocation()
+    ) {
+      node = null;
+    }
+  }
+
+  let scopeDepth = 0;
+  // Traverse the AST building up our text string.
+  while (node) {
+    switch (node.getType()) {
+      case ASTNode.types.INPUT: {
+        const connection = /** @type {!Connection} */ (node.getLocation());
+        if (!node.in()) {
+          pseudo.push(emptyFieldPlaceholder);
+        } else if (shouldAddParentheses(connection)) {
+          pseudo.push('(');
+        }
+        if (connection.type === 3) {
+          pseudo.push('{');
+          scopeDepth++;
+        }
+        break;
+      }
+      case ASTNode.types.FIELD: {
+        const field = /** @type {Field} */ (node.getLocation());
+        if (field.name !== constants.COLLAPSED_FIELD_NAME) {
+          pseudo.push(field.getText());
+        }
+        break;
+      }
+    }
+
+    const current = node;
+    node = current.in() || current.next();
+
+    if (!node) {
+      // Can't go in or next, keep going out until we can go next.
+      node = current.out();
+      checkRoot();
+      while (node && !node.next()) {
+        node = node.out();
+        checkRoot();
+        // If we hit an input on the way up, possibly close out parentheses.
+        if (node && node.getType() === ASTNode.types.INPUT) {
+          const connection = /** @type {!Connection} */ (node.getLocation());
+          if (shouldAddParentheses(connection)) {
+            pseudo.push(')');
+          }
+        }
+        if (node && node.getType() === ASTNode.types.BLOCK) {
+          if (current.getType() === ASTNode.types.NEXT) {
+            pseudo.push('}');
+            scopeDepth--;
+          }
+        }
+      }
+      if (node) {
+        node = node.next();
+      }
+    }
+  }
+
+  // Restore state of NAVIGATE_ALL_FIELDS.
+  ASTNode.NAVIGATE_ALL_FIELDS = prevNavigateFields;
+
+  // Run through our text array and simplify expression to remove parentheses
+  // around single field blocks.
+  // E.g. ['repeat', '(', '10', ')', 'times', 'do', '?']
+  for (let i = 2; i < pseudo.length; i++) {
+    if (pseudo[i - 2] === '(' && pseudo[i] === ')') {
+      pseudo[i - 2] = pseudo[i - 1];
+      pseudo.splice(i - 1, 2);
+    }
+  }
+
+  // Join the text array, removing spaces around added parentheses.
+  pseudo = pseudo.reduce((acc, value) => {
+    return acc + (acc.substr(-1) === '(' || value === ')' ? '' : ' ') + value;
+  }, '');
+  pseudo = pseudo.trim() || '???';
+
+  while (scopeDepth--) {
+    pseudo += '}';
+  }
+
+  return pseudo;
+};
+
+WorkspaceSvg.prototype.toPseudoCode = function (opt_emptyToken) {
+  return this.getTopBlocks()
+    .map(block => block.toPseudoCode(opt_emptyToken))
+    .join('\n');
 };
