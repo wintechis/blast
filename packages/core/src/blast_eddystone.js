c615e4a94327d19e9024150111a8591bd4badfaf
derwehr <derwehr@gmail.com>
2022-09-14 22:48:13 +0200

    fix object keys order

--- packages/core/src/blast_eddystone.js
+++ packages/core/src/blast_eddystone.js
@@ -19,19 +19,19 @@ import {throwError} from './blast_interpreter.js';
  * Eddystone Configuration Service and Characteristic UUIDs.
  */
 const UUIDS = {
-  CONFIG_SERVICE: 'a3c87500-8ed3-4bdf-8a39-a01bebede295',
-  CAPABILITIES_CHARACTERISTIC: 'a3c87501-8ed3-4bdf-8a39-a01bebede295',
   ACTIVE_SLOT_CHARACTERISTIC: 'a3c87502-8ed3-4bdf-8a39-a01bebede295',
-  ADVERTISING_INTERVAL_CHARACTERISTIC: 'a3c87503-8ed3-4bdf-8a39-a01bebede295',
-  RADIO_TX_POWER_CHARACTERISTIC: 'a3c87504-8ed3-4bdf-8a39-a01bebede295',
   ADVERTISED_TX_POWER_CHARACTERISTIC: 'a3c87505-8ed3-4bdf-8a39-a01bebede295',
-  LOCK_STATE_CHARACTERISTIC: 'a3c87506-8ed3-4bdf-8a39-a01bebede295',
-  UNLOCK_STATE_CHARACTERISTIC: 'a3c87507-8ed3-4bdf-8a39-a01bebede295',
-  PUBLIC_ECDH_KEY_CHARACTERISTIC: 'a3c87508-8ed3-4bdf-8a39-a01bebede295',
-  EID_IDENTITY_KEY_CHARACTERISTIC: 'a3c87509-8ed3-4bdf-8a39-a01bebede295',
+  ADVERTISING_INTERVAL_CHARACTERISTIC: 'a3c87503-8ed3-4bdf-8a39-a01bebede295',
   ADV_SLOT_DATA_CHARACTERISTIC: 'a3c8750a-8ed3-4bdf-8a39-a01bebede295',
+  CAPABILITIES_CHARACTERISTIC: 'a3c87501-8ed3-4bdf-8a39-a01bebede295',
+  CONFIG_SERVICE: 'a3c87500-8ed3-4bdf-8a39-a01bebede295',
+  EID_IDENTITY_KEY_CHARACTERISTIC: 'a3c87509-8ed3-4bdf-8a39-a01bebede295',
   FACTORY_RESET_CHARACTERISTIC: 'a3c8750b-8ed3-4bdf-8a39-a01bebede295',
+  LOCK_STATE_CHARACTERISTIC: 'a3c87506-8ed3-4bdf-8a39-a01bebede295',
+  PUBLIC_ECDH_KEY_CHARACTERISTIC: 'a3c87508-8ed3-4bdf-8a39-a01bebede295',
+  RADIO_TX_POWER_CHARACTERISTIC: 'a3c87504-8ed3-4bdf-8a39-a01bebede295',
   REMAIN_CONNECTABLE_CHARACTERISTIC: 'a3c8750c-8ed3-4bdf-8a39-a01bebede295',
+  UNLOCK_STATE_CHARACTERISTIC: 'a3c87507-8ed3-4bdf-8a39-a01bebede295',
 };
 
 export const eddystoneProperties = new Map();
@@ -85,6 +85,7 @@ export const getCapabilities = async function (webBluetoothId) {
  */
 export const parseCapabilities = function (dataView) {
   const capabilitiesArray = new Int8Array(dataView.buffer);
+  console.log(capabilitiesArray);
 
   // Parse the capabilities.
   const supportedTxPowerLevels = [];
