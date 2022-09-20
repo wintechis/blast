0b6766c6e578d2657c54dd5063ec16051069fa5c
zhouG42 <zhou.gui@fau.de>
2022-09-20 11:14:37 +0200

    update model mappings

--- packages/core/src/thingBlocks/text2code/mappings/word-context.js
+++ packages/core/src/thingBlocks/text2code/mappings/word-context.js
@@ -1,6 +1,6 @@
 export default {
-  num_encoder_tokens: 162,
-  num_decoder_tokens: 430,
-  encoder_max_seq_length: 9,
-  decoder_max_seq_length: 4,
-};
+  "num_encoder_tokens": 278,
+  "num_decoder_tokens": 355,
+  "encoder_max_seq_length": 10,
+  "decoder_max_seq_length": 4
+}
