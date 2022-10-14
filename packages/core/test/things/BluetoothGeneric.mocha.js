9f869a3e5b146173034089b6df8393b0e223dfcb
derwehr <derwehr@gmail.com>
2022-10-14 14:19:51 +0200

    upgrade BluetoothGeneric to new codecs

--- packages/core/test/things/BluetoothGeneric.mocha.js
+++ packages/core/test/things/BluetoothGeneric.mocha.js
@@ -36,14 +36,18 @@ suite('Bluetooth Generic', function () {
     const expectedTd = {
       '@context': [
         'https://www.w3.org/2019/wot/td/v1',
+        'https://www.w3.org/2022/wot/td/v1.1',
         {
-          '@language': 'en',
+          sbo: 'http://example.org/simple-bluetooth-ontology#',
+          bdo: 'http://example.org/binary-data-ontology#',
         },
+        {'@language': 'en'},
       ],
       '@type': ['Thing'],
       id: 'blast:bluetooth:BluetoothGeneric',
       title: 'Genereic Bluetooth device',
       description: 'A generic Bluetooth device',
+      'sbo:isConnectable': true,
       securityDefinitions: {
         nosec_sc: {
           scheme: 'nosec',
@@ -54,292 +58,332 @@ suite('Bluetooth Generic', function () {
       events: {},
       links: [],
       observedProperties: {},
-      subscribedEvents: {},
       properties: {
         barometricPressureTrend: {
+          title: 'Barometric pressure trend',
           description: 'The barometric pressure trend in hPa.',
-          forms: [
-            {
-              href: 'gatt://00001802-0000-1000-8000-00805f9b34fb/00002a1c-0000-1000-8000-00805f9b34fb/readNumber',
-              'wbt:id': 'deadbeef',
-            },
-          ],
+          unit: 'hPa',
+          type: 'number',
           observable: false,
           readOnly: true,
-          title: 'Barometric pressure trend',
-          type: 'number',
-          unit: 'hPa',
           writeOnly: false,
+          forms: [
+            {
+              href: `ble-web+gatt://deadbeef/00001802-0000-1000-8000-00805f9b34fb/00002a1c-0000-1000-8000-00805f9b34fb`,
+              op: 'readproperty',
+              'sbo:methodName': 'sbo:read',
+              contentType: 'application/x.binary-data-stream',
+            },
+          ],
         },
         batteryLevel: {
+          title: 'Battery level',
           description: 'Battery level in %.',
-          forms: [
-            {
-              href: 'gatt://0000180f-0000-1000-8000-00805f9b34fb/00002a19-0000-1000-8000-00805f9b34fb/readNumber',
-              'wbt:id': 'deadbeef',
-            },
-          ],
+          unit: '%',
+          type: 'number',
           observable: false,
           readOnly: true,
-          title: 'Battery level',
-          type: 'number',
-          unit: '%',
           writeOnly: false,
+          forms: [
+            {
+              href: `ble-web+gatt://deadbeef/0000180f-0000-1000-8000-00805f9b34fb/00002a19-0000-1000-8000-00805f9b34fb`,
+              op: 'readproperty',
+              'sbo:methodName': 'sbo:read',
+              contentType: 'application/x.binary-data-stream',
+            },
+          ],
         },
         deviceName: {
+          title: 'Device name',
           description: 'User defined name of the device',
-          forms: [
-            {
-              href: 'gatt://00001800-0000-1000-8000-00805f9b34fb/00002a00-0000-1000-8000-00805f9b34fb/writeWithoutResponse',
-              op: 'writeproperty',
-              'wbt:id': 'deadbeef',
-            },
-            {
-              href: 'gatt://00001800-0000-1000-8000-00805f9b34fb/00002a00-0000-1000-8000-00805f9b34fb/readText',
-              op: 'readproperty',
-              'wbt:id': 'deadbeef',
-            },
-          ],
+          type: 'string',
           observable: false,
           readOnly: false,
-          title: 'Device name',
-          type: 'string',
           writeOnly: false,
+          forms: [
+            {
+              href: `ble-web+gatt://deadbeef/00001800-0000-1000-8000-00805f9b34fb/00002a00-0000-1000-8000-00805f9b34fb`,
+              op: 'writeproperty',
+              'sbo:methodName': 'sbo:write-without-response',
+              contentType: 'application/x.binary-data-stream',
+            },
+            {
+              href: `ble-web+gatt://deadbeef/00001800-0000-1000-8000-00805f9b34fb/00002a00-0000-1000-8000-00805f9b34fb`,
+              op: 'readproperty',
+              'sbo:methodName': 'sbo:read',
+              contentType: 'application/x.binary-data-stream',
+            },
+          ],
         },
         elevation: {
+          title: 'Elevation',
           description: 'The elevation measured by the device.',
-          forms: [
-            {
-              href: 'gatt://00001803-0000-1000-8000-00805f9b34fb/00002a6c-0000-1000-8000-00805f9b34fb/readNumber',
-              'wbt:id': 'deadbeef',
-            },
-          ],
+          type: 'number',
           observable: false,
           readOnly: true,
-          title: 'Elevation',
-          type: 'number',
           writeOnly: false,
+          forms: [
+            {
+              href: `ble-web+gatt://deadbeef/00001803-0000-1000-8000-00805f9b34fb/00002a6c-0000-1000-8000-00805f9b34fb`,
+              op: 'readproperty',
+              'sbo:methodName': 'sbo:read',
+              contentType: 'application/x.binary-data-stream',
+            },
+          ],
         },
         firmwareRevision: {
+          title: 'Firmware revision',
           description: "Revision of the device's firmware",
-          forms: [
-            {
-              href: 'gatt://0000180a-0000-1000-8000-00805f9b34fb/00002a26-0000-1000-8000-00805f9b34fb/readText',
-              'wbt:id': 'deadbeef',
-            },
-          ],
+          type: 'string',
           observable: false,
           readOnly: true,
-          title: 'Firmware revision',
-          type: 'string',
           writeOnly: false,
+          forms: [
+            {
+              href: `ble-web+gatt://deadbeef/0000180a-0000-1000-8000-00805f9b34fb/00002a26-0000-1000-8000-00805f9b34fb`,
+              op: 'readproperty',
+              'sbo:methodName': 'sbo:read',
+              contentType: 'application/x.binary-data-stream',
+            },
+          ],
         },
         hardwareRevision: {
+          title: 'Hardware revision',
           description: "Revision of the device's hardware",
-          forms: [
-            {
-              href: 'gatt://0000180a-0000-1000-8000-00805f9b34fb/00002a27-0000-1000-8000-00805f9b34fb/readText',
-              'wbt:id': 'deadbeef',
-            },
-          ],
+          type: 'string',
           observable: false,
           readOnly: true,
-          title: 'Hardware revision',
-          type: 'string',
           writeOnly: false,
+          forms: [
+            {
+              href: `ble-web+gatt://deadbeef/0000180a-0000-1000-8000-00805f9b34fb/00002a27-0000-1000-8000-00805f9b34fb`,
+              op: 'readproperty',
+              'sbo:methodName': 'sbo:read',
+              contentType: 'application/x.binary-data-stream',
+            },
+          ],
         },
         humidity: {
-          description: 'The relative humidity in %',
-          forms: [
-            {
-              href: 'gatt://00001803-0000-1000-8000-00805f9b34fb/00002a6f-0000-1000-8000-00805f9b34fb/readNumber',
-              'wbt:id': 'deadbeef',
-            },
-          ],
-          observable: false,
-          readOnly: true,
           title: 'Humidity',
-          type: 'number',
+          description: 'The relative humidity in %',
           unit: '%',
-          writeOnly: false,
-        },
-        intermediateTemperature: {
-          description: 'The intermediate temperature measured by the device.',
-          forms: [
-            {
-              href: 'gatt://00001809-0000-1000-8000-00805f9b34fb/00002a1e-0000-1000-8000-00805f9b34fb/readNumber',
-              'wbt:id': 'deadbeef',
-            },
-          ],
+          type: 'number',
           observable: false,
           readOnly: true,
-          title: 'Intermediate temperature',
-          type: 'number',
           writeOnly: false,
+          forms: [
+            {
+              href: `ble-web+gatt://deadbeef/00001803-0000-1000-8000-00805f9b34fb/00002a6f-0000-1000-8000-00805f9b34fb`,
+              op: 'readproperty',
+              'sbo:methodName': 'sbo:read',
+              contentType: 'application/x.binary-data-stream',
+            },
+          ],
         },
         irradiance: {
+          title: 'Irradiance',
           description: 'Irradiance measured by the device.',
-          forms: [
-            {
-              href: 'gatt://00001803-0000-1000-8000-00805f9b34fb/00002a77-0000-1000-8000-00805f9b34fb/readNumber',
-              'wbt:id': 'deadbeef',
-            },
-          ],
+          type: 'number',
           observable: false,
           readOnly: true,
-          title: 'Irradiance',
-          type: 'number',
           writeOnly: false,
+          forms: [
+            {
+              href: `ble-web+gatt://deadbeef/00001803-0000-1000-8000-00805f9b34fb/00002a77-0000-1000-8000-00805f9b34fb`,
+              op: 'readproperty',
+              'sbo:methodName': 'sbo:read',
+              contentType: 'application/x.binary-data-stream',
+            },
+          ],
+        },
+        intermediateTemperature: {
+          title: 'Intermediate temperature',
+          description: 'The intermediate temperature measured by the device.',
+          type: 'number',
+          observable: false,
+          readOnly: true,
+          writeOnly: false,
+          forms: [
+            {
+              href: `ble-web+gatt://deadbeef/00001809-0000-1000-8000-00805f9b34fb/00002a1e-0000-1000-8000-00805f9b34fb`,
+              op: 'readproperty',
+              'sbo:methodName': 'sbo:read',
+              contentType: 'application/x.binary-data-stream',
+            },
+          ],
         },
         manufacturerName: {
-          forms: [
-            {
-              href: 'gatt://0000180a-0000-1000-8000-00805f9b34fb/00002a29-0000-1000-8000-00805f9b34fb/readText',
-              'wbt:id': 'deadbeef',
-            },
-          ],
-          observable: false,
-          readOnly: true,
           title: 'Manufacturer name',
           type: 'string',
+          observable: false,
+          readOnly: true,
           writeOnly: false,
+          forms: [
+            {
+              href: `ble-web+gatt://deadbeef/0000180a-0000-1000-8000-00805f9b34fb/00002a29-0000-1000-8000-00805f9b34fb`,
+              op: 'readproperty',
+              'sbo:methodName': 'sbo:read',
+              contentType: 'application/x.binary-data-stream',
+            },
+          ],
         },
         modelNumber: {
-          forms: [
-            {
-              href: 'gatt://0000180a-0000-1000-8000-00805f9b34fb/00002a24-0000-1000-8000-00805f9b34fb/readNumber',
-              'wbt:id': 'deadbeef',
-            },
-          ],
-          observable: false,
-          readOnly: true,
           title: 'Model number',
           type: 'number',
+          observable: false,
+          readOnly: true,
           writeOnly: false,
+          forms: [
+            {
+              href: `ble-web+gatt://deadbeef/0000180a-0000-1000-8000-00805f9b34fb/00002a24-0000-1000-8000-00805f9b34fb`,
+              op: 'readproperty',
+              'sbo:methodName': 'sbo:read',
+              contentType: 'application/x.binary-data-stream',
+            },
+          ],
         },
         movementCounter: {
+          title: 'Movement counter',
           description:
             'A counter incremented everytime the device starts moving.',
-          forms: [
-            {
-              href: 'gatt://00001809-0000-1000-8000-00805f9b34fb/00002a56-0000-1000-8000-00805f9b34fb/readNumber',
-              'wbt:id': 'deadbeef',
-            },
-          ],
+          type: 'number',
           observable: false,
           readOnly: true,
-          title: 'Movement counter',
-          type: 'number',
           writeOnly: false,
+          forms: [
+            {
+              href: `ble-web+gatt://deadbeef/00001809-0000-1000-8000-00805f9b34fb/00002a56-0000-1000-8000-00805f9b34fb`,
+              op: 'readproperty',
+              'sbo:methodName': 'sbo:read',
+              contentType: 'application/x.binary-data-stream',
+            },
+          ],
         },
         pressure: {
+          title: 'Pressure',
           description: 'Barometric pressure in hPa',
-          forms: [
-            {
-              href: 'gatt://00001809-0000-1000-8000-00805f9b34fb/00002a6d-0000-1000-8000-00805f9b34fb/readNumber',
-              'wbt:id': 'deadbeef',
-            },
-          ],
+          unit: 'hPa',
+          type: 'number',
           observable: false,
           readOnly: true,
-          title: 'Pressure',
-          type: 'number',
-          unit: 'hPa',
           writeOnly: false,
+          forms: [
+            {
+              href: `ble-web+gatt://deadbeef/00001809-0000-1000-8000-00805f9b34fb/00002a6d-0000-1000-8000-00805f9b34fb`,
+              op: 'readproperty',
+              'sbo:methodName': 'sbo:read',
+              contentType: 'application/x.binary-data-stream',
+            },
+          ],
         },
         serialNumber: {
-          forms: [
-            {
-              href: 'gatt://0000180a-0000-1000-8000-00805f9b34fb/00002a25-0000-1000-8000-00805f9b34fb/readText',
-              'wbt:id': 'deadbeef',
-            },
-          ],
-          observable: false,
-          readOnly: true,
           title: 'Serial number',
           type: 'string',
+          observable: false,
+          readOnly: true,
           writeOnly: false,
+          forms: [
+            {
+              href: `ble-web+gatt://deadbeef/0000180a-0000-1000-8000-00805f9b34fb/00002a25-0000-1000-8000-00805f9b34fb`,
+              op: 'readproperty',
+              'sbo:methodName': 'sbo:read',
+              contentType: 'application/x.binary-data-stream',
+            },
+          ],
         },
         softwareRevision: {
+          title: 'Software revision',
           description: "Revision of the device's software",
-          forms: [
-            {
-              href: 'gatt://0000180a-0000-1000-8000-00805f9b34fb/00002a28-0000-1000-8000-00805f9b34fb/readText',
-              'wbt:id': 'deadbeef',
-            },
-          ],
+          type: 'string',
           observable: false,
           readOnly: true,
-          title: 'Software revision',
-          type: 'string',
           writeOnly: false,
+          forms: [
+            {
+              href: `ble-web+gatt://deadbeef/0000180a-0000-1000-8000-00805f9b34fb/00002a28-0000-1000-8000-00805f9b34fb`,
+              op: 'readproperty',
+              'sbo:methodName': 'sbo:read',
+              contentType: 'application/x.binary-data-stream',
+            },
+          ],
         },
         temperature: {
-          forms: [
-            {
-              href: 'gatt://00001809-0000-1000-8000-00805f9b34fb/00002a6e-0000-1000-8000-00805f9b34fb/readNumber',
-              'wbt:id': 'deadbeef',
-            },
-          ],
-          observable: false,
-          readOnly: true,
           title: 'Temperature',
           type: 'number',
+          observable: false,
+          readOnly: true,
           writeOnly: false,
+          forms: [
+            {
+              href: `ble-web+gatt://deadbeef/00001809-0000-1000-8000-00805f9b34fb/00002a6e-0000-1000-8000-00805f9b34fb`,
+              op: 'readproperty',
+              'sbo:methodName': 'sbo:read',
+              contentType: 'application/x.binary-data-stream',
+            },
+          ],
         },
         temperatureMeasurement: {
-          forms: [
-            {
-              href: 'gatt://00001809-0000-1000-8000-00805f9b34fb/00002a1c-0000-1000-8000-00805f9b34fb/readNumber',
-              'wbt:id': 'deadbeef',
-            },
-          ],
-          observable: false,
-          readOnly: true,
           title: 'Temperature measurement',
           type: 'number',
+          observable: false,
+          readOnly: true,
           writeOnly: false,
+          forms: [
+            {
+              href: `ble-web+gatt://deadbeef/00001809-0000-1000-8000-00805f9b34fb/00002a1c-0000-1000-8000-00805f9b34fb`,
+              op: 'readproperty',
+              'sbo:methodName': 'sbo:read',
+              contentType: 'application/x.binary-data-stream',
+            },
+          ],
         },
         temperatureType: {
-          forms: [
-            {
-              href: 'gatt://00001809-0000-1000-8000-00805f9b34fb/00002a1d-0000-1000-8000-00805f9b34fb/readText',
-              'wbt:id': 'deadbeef',
-            },
-          ],
-          observable: false,
-          readOnly: true,
           title: 'Temperature type',
           type: 'string',
-          writeOnly: false,
-        },
-        txPowerLevel: {
-          forms: [
-            {
-              href: 'gatt://00001804-0000-1000-8000-00805f9b34fb/00002a07-0000-1000-8000-00805f9b34fb/readText',
-              'wbt:id': 'deadbeef',
-            },
-          ],
           observable: false,
           readOnly: true,
+          writeOnly: false,
+          forms: [
+            {
+              href: `ble-web+gatt://deadbeef/00001809-0000-1000-8000-00805f9b34fb/00002a1d-0000-1000-8000-00805f9b34fb`,
+              op: 'readproperty',
+              'sbo:methodName': 'sbo:read',
+              contentType: 'application/x.binary-data-stream',
+            },
+          ],
+        },
+        txPowerLevel: {
           title: 'Tx Power Level',
           type: 'number',
+          observable: false,
+          readOnly: true,
           writeOnly: false,
+          forms: [
+            {
+              href: `ble-web+gatt://deadbeef/00001804-0000-1000-8000-00805f9b34fb/00002a07-0000-1000-8000-00805f9b34fb`,
+              op: 'readproperty',
+              'sbo:methodName': 'sbo:read',
+              contentType: 'application/x.binary-data-stream',
+            },
+          ],
         },
         weight: {
           title: 'Weight',
           type: 'number',
-          forms: [
-            {
-              href: 'gatt://00001808-0000-1000-8000-00805f9b34fb/00002a9d-0000-1000-8000-00805f9b34fb/readNumber',
-              'wbt:id': 'deadbeef',
-            },
-          ],
           observable: false,
           readOnly: true,
           writeOnly: false,
+          forms: [
+            {
+              href: `ble-web+gatt://deadbeef/00001808-0000-1000-8000-00805f9b34fb/00002a9d-0000-1000-8000-00805f9b34fb`,
+              op: 'readproperty',
+              'sbo:methodName': 'sbo:read',
+              contentType: 'application/x.binary-data-stream',
+            },
+          ],
         },
       },
+      subscribedEvents: {},
     };
     expect(actualTd).to.deep.equal(expectedTd);
   });
