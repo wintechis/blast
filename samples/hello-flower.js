55a6a9584423a0914bf044a5badce214db625ae2
Andreas Harth <andreas.harth@fau.de>
2022-10-17 16:13:27 +0200

    api tweak

--- samples/hello-flower.js
+++ samples/hello-flower.js
@@ -1,13 +1,22 @@
-import blast;
+from blast import driver.xiaomiflowercare;
 
-fc = blast.connect('00:11:22:33:44');
+fc = driver.xiaomiflowercare.createThing('00:11:22:33:44');
 
+/**
+ * main
+ */
 async function main() {
     var temp;
+
+    fc.connect();
+
+    // what happens if there is a disconnect in between?
     while (true) {
 	await temp = fc.readProperty('temperature');
 	console.log('temperature is ', temp);
     }
+
+    fc.disconnect();
 }
 
 main();
