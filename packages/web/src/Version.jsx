8feab7c4646555215c60fbc17e6866618a8d639a
derwehr <derwehr@gmail.com>
2022-10-31 17:08:29 +0100

    add missing devDependencies

--- packages/web/src/Version.jsx
+++ packages/web/src/Version.jsx
@@ -11,7 +11,7 @@ export default function Version() {
   });
 
   // This constant will be updated by gulp on build
-  const rev = '#f78cde8';
+  const rev = '#59cb6b9';
 
   const {vertical, horizontal, open} = state;
 
