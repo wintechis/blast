1467bd5dce74e4678982cc3bea50d4b280b56712
Freund, Michael <michael.freund@iis.fraunhofer.de>
2023-03-06 12:07:09 +0100

    Change action brigthness to dim

--- packages/web/src/Version.jsx
+++ packages/web/src/Version.jsx
@@ -11,7 +11,7 @@ export default function Version() {
   });
 
   // This constant will be updated by gulp on build
-  const rev = '#f423ea2';
+  const rev = '#fc1c631';
 
   const {vertical, horizontal, open} = state;
 
