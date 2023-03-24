5928b1d6acf3f4a138acdcbc840b96113f5e6615
derwehr <derwehr@gmail.com>
2023-03-24 11:02:25 +0100

    update lockfile

--- packages/web/src/Version.jsx
+++ packages/web/src/Version.jsx
@@ -11,7 +11,7 @@ export default function Version() {
   });
 
   // This constant will be updated by gulp on build
-  const rev = '#d90eb98';
+  const rev = '#6fdeaee';
 
   const {vertical, horizontal, open} = state;
 
