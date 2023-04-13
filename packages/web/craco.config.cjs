4d4f1b557d42f9f45847b8f3ce990a4e86e2f9eb
derwehr <derwehr@gmail.com>
2023-04-13 15:35:40 +0200

    suppress sourceMap warnings

--- packages/web/craco.config.cjs
+++ packages/web/craco.config.cjs
@@ -6,6 +6,18 @@ module.exports = {
   },
 
   webpack: {
+    configure: {
+      ignoreWarnings: [
+        function ignoreSourcemapsloaderWarnings(warning) {
+          return (
+            warning.module &&
+            warning.module.resource.includes('node_modules') &&
+            warning.details &&
+            warning.details.includes('source-map-loader')
+          );
+        },
+      ],
+    },
     resolve: {
       alias: {
         '@mui/styled-engine': '@mui/styled-engine-sc',
