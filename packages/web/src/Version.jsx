fde9a04807a686447e8a1364d9c4d123453ee637
derwehr <derwehr@gmail.com>
2023-05-17 09:37:08 +0200

    re-build

--- packages/web/src/Version.jsx
+++ packages/web/src/Version.jsx
@@ -11,7 +11,7 @@ export default function Version() {
   });
 
   // This constant will be updated by gulp on build
-  const rev = '#2d614a7';
+  const rev = '#902d2f3';
 
   const {vertical, horizontal, open} = state;
 
