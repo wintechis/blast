2dd46a2a31606b6a2171228ca5d1e0526ebf2faf
derwehr <derwehr@gmail.com>
2023-05-25 11:06:47 +0200

    re-add `.yarnrc.yml`

--- packages/web/src/Version.jsx
+++ packages/web/src/Version.jsx
@@ -11,7 +11,7 @@ export default function Version() {
   });
 
   // This constant will be updated by gulp on build
-  const rev = '#3120504';
+  const rev = '#cdcc044';
 
   const {vertical, horizontal, open} = state;
 
