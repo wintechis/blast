9d149b9c8f5bd61894766f69b2f462c3f7ac18eb
derwehr <derwehr@gmail.com>
2023-06-15 13:18:57 +0200

    update node-wot

--- packages/web/src/Version.jsx
+++ packages/web/src/Version.jsx
@@ -11,7 +11,7 @@ export default function Version() {
   });
 
   // This constant will be updated by gulp on build
-  const rev = '#4eb77a9';
+  const rev = '#37f467f';
 
   const {vertical, horizontal, open} = state;
 
