02fdb27f6ac24668a441da89b04eb8d6d1139dab
derwehr <derwehr@gmail.com>
2023-06-29 17:47:25 +0200

    fix gamepad pro `joystick` event

--- packages/web/src/Version.jsx
+++ packages/web/src/Version.jsx
@@ -11,7 +11,7 @@ export default function Version() {
   });
 
   // This constant will be updated by gulp on build
-  const rev = '#4b00aa4';
+  const rev = '#4ea609b';
 
   const {vertical, horizontal, open} = state;
 
