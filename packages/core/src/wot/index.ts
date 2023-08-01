b0faaccc8147d6d09d23d66650d149e163fec433
derwehr <derwehr@gmail.com>
2023-08-01 15:40:18 +0200

    add `readProperty` to `consume` overwrite

--- packages/core/src/wot/index.ts
+++ packages/core/src/wot/index.ts
@@ -207,6 +207,24 @@ const ownConsume = function (exposedThing: ExposedThing): WoT.ConsumedThing {
     return output;
   };
 
+  consumedThing.readProperty = async function (
+    propertyName: string,
+    options: WoT.InteractionOptions = {}
+  ): Promise<InteractionOutput> {
+    // add a dummy form to by-pass protocol-listener-registry check
+    const form = {
+      op: 'readProperty',
+    } as FormElementBase;
+    exposedThing.properties[propertyName].forms = [form];
+
+    const outputContent = await exposedThing.handleReadProperty(propertyName, {
+      formIndex: 0,
+      ...options,
+    });
+    const output = new InteractionOutput(outputContent);
+    return output;
+  };
+
   return consumedThing;
 };
 
