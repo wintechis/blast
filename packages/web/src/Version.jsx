da66dc34b79db411039041f726682ddaecba800a
derwehr <derwehr@gmail.com>
2023-10-31 13:48:57 +0100

    use latest yarn 3 version instead of yarn berry

--- packages/web/src/Version.jsx
+++ packages/web/src/Version.jsx
@@ -11,7 +11,7 @@ export default function Version() {
   });
 
   // This constant will be updated by gulp on build
-  const rev = '#9f5fc70';
+  const rev = '#98ac131';
 
   const {vertical, horizontal, open} = state;
 
