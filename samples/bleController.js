12b386299fe547113757c3a00f5d2169621580d5
derwehr <derwehr@gmail.com>
2023-11-04 17:51:31 +0100

    add licenses summary

--- samples/bleController.js
+++ samples/bleController.js
@@ -5,6 +5,8 @@ const mac = 'BE5860018744';
 
 const thing = await createThing(BleRgbController, mac);
 
+await thing.writeProperty('power', {is_on: 0});
+
 while (true) {
     await thing.writeProperty('colour', {R: 255, G: 0, B: 0});
     await thing.writeProperty('colour', {R: 0, G: 255, B: 0});
