0ed9252414be66db6252aaba87370a79c5ebe151
derwehr <derwehr@gmail.com>
2024-01-19 17:02:46 +0100

    rename td file

--- packages/web/src/Version.jsx
+++ packages/web/src/Version.jsx
@@ -11,7 +11,7 @@ export default function Version() {
   });
 
   // This constant will be updated by gulp on build
-  const rev = '#613ee62';
+  const rev = '#829ded4';
 
   const {vertical, horizontal, open} = state;
 
