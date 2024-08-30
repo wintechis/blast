5f98916513a54befc4b3aae2f24d942d9973b820
Thomas Wehr <derwehr@gmail.com>
2024-08-30 10:47:45 +0200

    Remove unused files

--- dist/WebBluetoothAdapter.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="web-bluetooth" />
-import { BluetoothAdapter } from '@blast/core';
-export default class ConcreteBluetoothAdapter extends BluetoothAdapter {
-    getCharacteristic(deviceId: string, serviceId: BluetoothServiceUUID, characteristicId: BluetoothCharacteristicUUID): Promise<BluetoothRemoteGATTCharacteristic>;
-    observeGAP(deviceId: string, handler: (event: any) => void): Promise<void>;
-}
