00c3468ce16ae773f3f22b936a84d4890713e4eb
Thomas Wehr <derwehr@gmail.com>
2025-04-16 13:44:29 +0200

    fix peer dependency issues

--- packages/browser-demo/src/Version.jsx
+++ packages/browser-demo/src/Version.jsx
@@ -11,7 +11,7 @@ export default function Version() {
   });
 
   // This constant will be updated by gulp on build
-  const rev = '#6cc24d5';
+  const rev = '#38f94e1';
 
   const {vertical, horizontal, open} = state;
 
